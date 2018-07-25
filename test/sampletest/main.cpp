4db16800ba0592371e9f11ee9fbe4e7897bcb9c8
Dawid Wdowiak <dwdw9@wp.pl>
2018-07-25 19:55:02 +0200

    Added sample google test

---
+++ test/sampletest/main.cpp
@@ -0,0 +1,13 @@
+//#include "src/matrix.h"
+#include "gtest/gtest.h"
+
+
+int main(int argc, char **argv) {
+	::testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
+}
+
+TEST(SampleTest, Sample)
+{
+	EXPECT_EQ(0, 1);
+}
